32f6c97fc93434c7e72ec12dc97d854e8856d640
Đăng Khoa <khoaace@gmail.com>
2018-04-25 21:28:10 +0700

    update

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,17 +1,13 @@
 module.exports.calc = function (first_Num,second_Num,method_calc1) {
 
         var result_calc;
-    if(isNaN(first_Num))
+    if(isNaN(first_Num)|| (first_Num.trim()==""))
     {
         return "Giá trị ở ô thứ nhất không phải là số";
     }
-    if(isNaN(second_Num))
+    if(isNaN(second_Num)|| (second_Num.trim()==""))
     {
         return  "Giá trị ở ô thứ hai không phải là số";
-    }
-    if((first_Num=="") || (second_Num==""))
-    {
-        return "Ô nhập số không được để rỗng";
     }
     if((method_calc1 != "add" )&&(method_calc1 != "sub" )&&(method_calc1 != "mult" )&&(method_calc1 != "divi" ))
     {
@@ -28,9 +24,13 @@ module.exports.calc = function (first_Num,second_Num,method_calc1) {
             case "mult":
                 result_calc = Number(first_Num) * Number(second_Num);
                 break;
-            case "divi":
+            case "divi": {
+                if(second_Num =="0")
+                    return "Không thể chia cho 0.";
+                else
                 result_calc = Number(first_Num) / Number(second_Num);
                 break;
+            }
         }
         return result_calc;
 
